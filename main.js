e3b879937dab7d49f99a695c48009457370f811e
Tommi Tuura <tommi.tuura@gmail.com>
2013-02-09 03:53:24 +0200

    adding listeners to canvas and window

--- main.js
+++ main.js
@@ -2,5 +2,22 @@
 
 window.onload = function() {
 	game.ctx = document.getElementById('mycanvas').getContext('2d');
+
+	$(document).keydown(function(event) {
+		game.controls.handlers.keydown(event);
+	});
+	$(document).keyup(function(eventInformation) {
+		game.controls.handlers.keyup(event);
+	});
+	$('#mycanvas').mousedown(function(event) {
+		game.controls.handlers.mouseclick(event);
+	});
+	$('#mycanvas').mousemove(function(event) {
+		game.controls.handlers.mousemove(event);
+	});
+	$('#mycanvas').mouseout(function(event) {
+		game.controls.handlers.mouseout();
+	});
+
 	game.engine.main.tick();
 };

d673742edd63fe98c41f64fd94dd63edc1a532e7
Tommi Tuura <tommi.tuura@gmail.com>
2013-02-11 17:07:03 +0200

    tabs to space

--- engine.js
+++ engine.js
@@ -1,114 +1,114 @@
 "use strict";
 
 window.requestAnimFrame = (function() {
-	return window.requestAnimationFrame   ||
-	   window.webkitRequestAnimationFrame || 
-	   window.mozRequestAnimationFrame    || 
-	   window.oRequestAnimationFrame      || 
-	   window.msRequestAnimationFrame     || 
-	   function(callback, element){
-	       window.setTimeout(callback, 1000 / 60);
-	   };
+    return window.requestAnimationFrame   || 
+       window.webkitRequestAnimationFrame || 
+       window.mozRequestAnimationFrame    || 
+       window.oRequestAnimationFrame      || 
+       window.msRequestAnimationFrame     || 
+       function(callback, element){
+           window.setTimeout(callback, 1000 / 60);
+       };
 })();
 
 game.engine = {};
 
 game.engine.startScreen = (function() {
-	var frames = 0;
-	var x = 200;
-	var y = 200;
+    var frames = 0;
+    var x = 200;
+    var y = 200;
 
-	var tick = function() {
-		frames++;
-		var move = game.controls.getMovement();
-		x += move[0];
-		y += move[1];
-	};
-	var draw = function() {
-		game.ctx.clearRect(0,0,game.ctx.canvas.width,game.ctx.canvas.height);
-		game.ctx.fillStyle = "20pt Arial Sans-serif";
-		game.ctx.fillText("Start Screen: " + frames,x,y);
-	};
+    var tick = function() {
+        frames++;
+        var move = game.controls.getMovement();
+        x += move[0];
+        y += move[1];
+    };
+    var draw = function() {
+        game.ctx.clearRect(0,0,game.ctx.canvas.width,game.ctx.canvas.height);
+        game.ctx.fillStyle = "20pt Arial Sans-serif";
+        game.ctx.fillText("Start Screen: " + frames,x,y);
+    };
 
-	return {
-		tick: tick,
-		draw: draw
-	}
+    return {
+        tick: tick,
+        draw: draw
+    }
 })();
 
 game.engine.gameScreen = (function() {
-	var frames = 0;
-	var x = 200;
-	var y = 200;
-	var tick = function() {
-		frames++;
-		var move = game.controls.getMovement();
-		x += move[0];
-		y += move[1];
-	};
-	var draw = function() {
-		game.ctx.clearRect(0,0,game.ctx.canvas.width,game.ctx.canvas.height);
-		game.ctx.fillStyle = "20pt Arial Sans-serif";
-		game.ctx.fillText("Game Screen: " + frames,x,y);
-	};
+    var frames = 0;
+    var x = 200;
+    var y = 200;
+    var tick = function() {
+        frames++;
+        var move = game.controls.getMovement();
+        x += move[0];
+        y += move[1];
+    };
+    var draw = function() {
+        game.ctx.clearRect(0,0,game.ctx.canvas.width,game.ctx.canvas.height);
+        game.ctx.fillStyle = "20pt Arial Sans-serif";
+        game.ctx.fillText("Game Screen: " + frames,x,y);
+    };
 
-	return {
-		tick: tick,
-		draw: draw
-	}
+    return {
+        tick: tick,
+        draw: draw
+    }
 })();
 
 game.engine.main = (function() {
-	var currentMode = game.engine.startScreen;
-	var timerDebug = false;
+    var currentMode = game.engine.startScreen;
+    var timerDebug = false;
 
-	var frameNum = 0;
-	var frametime = {
-		start: null,
-		prev: null,
-		now: null,
-		current: null,
-		sum: 0,
-		maxVal: 0
-	};
+    var frameNum = 0;
+    var frametime = {
+        start: null,
+        prev: null,
+        now: null,
+        current: null,
+        sum: 0,
+        maxVal: 0
+    };
 
-	var debug = function(debugging) {
-		if (typeof debugging!=='boolean') { timerDebug = !timerDebug; } 
-		else { timerDebug = debugging; }
-	}
+    var debug = function(debugging) {
+        if (typeof debugging!=='boolean') { timerDebug = !timerDebug; } 
+        else { timerDebug = debugging; }
+    }
 
-	var state = function(state) {
-		if (state==="game") {
-			currentMode = game.engine.gameScreen;
-		} else {
-			currentMode = game.engine.startScreen;
-		}
-	}
+    var state = function(state) {
+        if (state==="game") {
+            currentMode = game.engine.gameScreen;
+        } else {
+            currentMode = game.engine.startScreen;
+        }
+    }
 
-	var tick = function() {
-		frametime.start = new Date().valueOf();
-		
-		currentMode.tick();
-		currentMode.draw();
+    var tick = function() {
+        frametime.start = new Date().valueOf();
+        
+        currentMode.tick();
+        currentMode.draw();
 
-		frameNum++;
-		if (frameNum > 59) {
-			if (timerDebug) { console.log('Average % of allowed time used by frames:', frametime.sum/60, '\nHighest % of allowed time used by a frame:', frametime.maxVal); }
-			frameNum = 0;
-			frametime.sum = 0;
-			frametime.maxVal = 0;
-		}
-		frametime.prev = frametime.now;
-		frametime.now = new Date().valueOf();
-		frametime.current = ((frametime.now - frametime.start)/(frametime.now - frametime.prev) * 100);
-		frametime.sum = frametime.sum + frametime.current;
-		if (frametime.current > frametime.maxVal) { frametime.maxVal = frametime.current; }
-		requestAnimFrame(game.engine.main.tick);
-	}
+        frameNum++;
+        if (frameNum > 59) {
+            if (timerDebug) { console.log('Average % of allowed time used by frames:', frametime.sum/60, '\nHighest % of allowed time used by a frame:', frametime.maxVal); }
+            frameNum = 0;
+            frametime.sum = 0;
+            frametime.maxVal = 0;
+        }
+        frametime.prev = frametime.now;
+        frametime.now = new Date().valueOf();
+        frametime.current = ((frametime.now - frametime.start)/(frametime.now - frametime.prev) * 100);
+        frametime.sum = frametime.sum + frametime.current;
+        if (frametime.current > frametime.maxVal) { frametime.maxVal = frametime.current; }
+        requestAnimFrame(game.engine.main.tick);
+    }
 
-	return {
-		debug: debug,
-		state: state, 
-		tick: tick
-	}
+    return {
+        debug: debug,
+        state: state, 
+        tick: tick
+    }
 })();
--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 "use strict";
 
 window.onload = function() {
-	game.ctx = document.getElementById('mycanvas').getContext('2d');
+    game.ctx = document.getElementById('mycanvas').getContext('2d');
 
-	$(document).keydown(function(event) {
-		game.controls.handlers.keydown(event);
-	});
-	$(document).keyup(function(eventInformation) {
-		game.controls.handlers.keyup(event);
-	});
-	$('#mycanvas').mousedown(function(event) {
-		game.controls.handlers.mouseclick(event);
-	});
-	$('#mycanvas').mousemove(function(event) {
-		game.controls.handlers.mousemove(event);
-	});
-	$('#mycanvas').mouseout(function(event) {
-		game.controls.handlers.mouseout();
-	});
-
-	game.engine.main.tick();
+    $(document).keydown(function(event) {
+        game.controls.handlers.keydown(event);
+    });
+    $(document).keyup(function(eventInformation) {
+        game.controls.handlers.keyup(event);
+    });
+    $('#mycanvas').mousedown(function(event) {
+        game.controls.handlers.mouseclick(event);
+    });
+    $('#mycanvas').mousemove(function(event) {
+        game.controls.handlers.mousemove(event);
+    });
+    $('#mycanvas').mouseout(function(event) {
+        game.controls.handlers.mouseout();
+    });
+    
+    game.engine.main.tick();
 };
